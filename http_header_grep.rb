d93c43a1f803ce1452c79e7df4ef75c7a4a88461
ethicalhack3r <ryandewhurst@gmail.com>
2015-06-15 16:55:56 +0200

    Use new hash format

--- http_header_grep.rb
+++ http_header_grep.rb
@@ -20,11 +20,11 @@ urls.each_with_index do |url, index|
   url = URI.parse(URI.encode(url))
 
   response = Typhoeus.get( url.to_s,
-                           :ssl_verifyhost => 0,
-                           :ssl_verifypeer => false,
-                           :followlocation => true,
-                           :headers => {'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0'},
-                           :timeout => 1000 )
+                           ssl_verifyhost: 0,
+                           ssl_verifypeer: false,
+                           followlocation: true,
+                           headers: {'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0'},
+                           timeout: 1000 )
 
   matches = response.response_headers.scan(Regexp.new(regex, true))
 
